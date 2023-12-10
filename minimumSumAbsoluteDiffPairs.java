a870cc8710b6ed33cd3a08cf003e82542ed80471
Ankit Kumar Jha <ankitjha2701@gmail.com>
2023-12-10 21:01:57 +0530

    Create minimumSumAbsoluteDiffPairs.java

---
+++ minimumSumAbsoluteDiffPairs.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+public class file5 {
+    public static void main(String[] args) {
+        int A[]={1,2,3};
+        int B[]={2,1,3};
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int minDiff=0;
+        for(int i=0;i<A.length;i++){
+            minDiff=minDiff+Math.abs(A[i]-B[i]);
+        }
+        System.out.println(minDiff);
+    }
